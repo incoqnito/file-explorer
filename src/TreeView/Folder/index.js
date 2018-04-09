e75aeb253a0e39a9348d82b83848d23760af3748
DragonRaider5 <marces.engel@googlemail.com>
2018-04-09 11:04:26 +0200

    Styling

--- src/TreeView/Folder/index.js
+++ src/TreeView/Folder/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import FolderIcon from '../icons/Folder.svg'
+import OpenFolderIcon from '../icons/OpenFolder.svg'
+
 const Folder = ({ folder: { title, id, children }, shouldDisplay, onExpand, path = [] }) => shouldDisplay(path) && (
   <Wrapper className='file-explorer-tree-view-folder' onClick={(event) => {
     event.stopPropagation()
     onExpand(path.concat(id))
   }}>
+    <Icon src={shouldDisplay(path.concat(id)) ? OpenFolderIcon : FolderIcon} fill='#333' />
+
+    <Title>
     { title }
+    </Title>
 
     { children && children.map((folder) => (
       <Folder
@@ -22,7 +29,32 @@ const Folder = ({ folder: { title, id, children }, shouldDisplay, onExpand, path
 
 const Wrapper = styled.div`
   margin-top: 8px;
-  margin-left: 8px;
+  margin-left: 12px;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  cursor: pointer;
+
+  & > * {
+    vertical-align: middle;
+  }
+`
+
+const Icon = ({ src: Icon, ...props }) => (
+  <Icon width='16' height='16' {...props} />
+)
+
+const Title = styled.span`
+  position: relative;
+  color: #333;
+  padding-left: 4px;
+
+  font-size: 14px;
+  font-weight: 200;
+
+  white-space: nowrap;
+  user-select: none;
 `
 
 export default Folder
--- src/TreeView/index.js
+++ src/TreeView/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Folder from './Folder/index.js'
 
@@ -38,13 +39,28 @@ export default class TreeView extends React.PureComponent {
   render () {
     const { data } = this.props
 
-    return data.map((folder) => (
-      <Folder
-        key={folder.id}
-        folder={folder}
-        shouldDisplay={this.shouldDisplayFolder}
-        onExpand={this.onExpandFolder}
-      />
-    ))
+    return (
+      <Wrapper>
+        {
+          data.map((folder) => (
+            <Folder
+              key={folder.id}
+              folder={folder}
+              shouldDisplay={this.shouldDisplayFolder}
+              onExpand={this.onExpandFolder}
+            />
+          ))
+        }
+      </Wrapper>
+    )
   }
 }
+
+const Wrapper = styled.div`
+  * {
+    font-family: sans-serif;
+  }
+
+  border: 1px solid #CCC;
+  padding-bottom: 8px;
+`
